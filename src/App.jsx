5470281c068642b97fb762229fece9b0b946d833
Anthony DiBernardo <adibern2@charlotte.edu>
2025-11-20 15:58:56 -0500

    feat: added fix for last bits of login

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import './App.css'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import { UserProvider } from './components/Provider.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
 
   return (
     <UserProvider>
-        <Header />
+        <BrowserRouter>
+            <Header />
+        </BrowserRouter>
         <Footer />
     </UserProvider>
   )
